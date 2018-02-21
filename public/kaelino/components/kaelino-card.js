79879b324cd44e8bcd4942d2db66859a118aa2ef
alexlockhart7 <alex.lockhart@icfolson.com>
2018-02-21 10:58:55 -0500

    Add events to the card component.

--- public/kaelino/components/kaelino-card.js
+++ public/kaelino/components/kaelino-card.js
@@ -5,21 +5,34 @@ export default class KaelinoCard extends HTMLElement {
     return "kaelino-card";
   }
 
+  get actionText() { return this._actionText }
+  set actionText(actionText) { this._actionText = actionText; this.render(); }
+
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
     this.shadowHyper = hyper.bind(this.shadowRoot);
+    this._actionText = this.getAttribute("actionText");
   }
 
   connectedCallback() {
     this.render();
   }
 
+  barClicked() {
+    this.dispatchEvent(new CustomEvent("kaelino-card-submit"));
+  }
+
   render() {
     this.shadowHyper`
       ${this._styles()}
-      <slot></slot>
-      <p>Hello, I am from hyperHtml ABC</p>
+      <div class="container">
+        <slot></slot>
+        <p>Hello, I am from hyperHtml ABC</p>
+      </div>
+      <div class="bar" onclick=${() => this.barClicked()}>
+        ${this.actionText}
+      </div>
     `
   }
 
@@ -27,12 +40,31 @@ export default class KaelinoCard extends HTMLElement {
     return hyper`<style>
       :host {
         display: block;
-        background: white;
-        max-width: 360px;
         margin: 100px auto 16px;
-        padding: 32px 24px;
+        max-width: 360px;
         border-radius: 3px;
         box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+      }
+
+      .container {
+        background: white;
+        padding: 32px 24px;
+      }
+
+      .bar {
+        width: 100%;
+        color: white;
+        padding: 15px;
+        box-sizing: border-box;
+        text-align: center;
+        background: #039be5;
+        transition: background-color 0.3s;
+        border-radius: 0 0 3px 3px;
+        cursor: pointer;
+      }
+
+      .bar:hover {
+        background-color: #097fb9;
       }
     </style>`;
   }
